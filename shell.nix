bc64d7c9d68529799cb6c231ffef54a958bcd129
David Izquierdo <david@izquierdofernandez.com>
2018-05-15 10:48:22 +0200

    add basic shell.nix

---
+++ shell.nix
@@ -0,0 +1,11 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation rec {
+  name = "env";
+  env = buildEnv { name = "pod.rs"; paths = buildInputs; };
+  buildInputs = [
+    pkgconfig
+    openssl_1_1_0
+    cargo
+    rustc
+  ];
+}
