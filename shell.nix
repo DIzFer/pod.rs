96d93d7725a365870bbb8770b0cbfd3e11d92729
David Izquierdo <david@izquierdofernandez.com>
2018-10-08 10:40:12 +0200

    update deps, fix openssl in shell.nix

--- shell.nix
+++ shell.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation rec {
   env = buildEnv { name = "pod.rs"; paths = buildInputs; };
   buildInputs = [
     pkgconfig
-    openssl_1_1_0
+    openssl_1_0_2
     cargo
     rustc
     rustfmt
